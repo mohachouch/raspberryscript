02ce479963dbf0fbcf5cc986ebe08d151ab8fa0f
Mohamed CHOUCHANE <mohamed.chouchane1990@gmail.com>
2018-04-22 12:42:11 +0200

    Update setup.sh

--- LICENSE.md/setup.sh
+++ LICENSE.md/setup.sh
@@ -8,9 +8,14 @@ unclutter \
 vim \
 && apt-get autoremove -y && apt-get clean
 
+# change language
+locale-gen fr_FR.UTF-8 UTF-8
+update-locale fr_FR.UTF-8 UTF-8
+export LANGUAGE=fr_FR.UTF-8
+export LC_ALL=fr_FR.UTF-8
+
 # fix resolution
 echo "disable_overscan=1" >> /boot/config.txt
 
 # disable screensaver
 sed -i "s/\[SeatDefaults\]\n/\[SeatDefaults\]\nxserver-command=X -s 0 -dpms/g" /etc/lightdm/lightdm.conf
-
